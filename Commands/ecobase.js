2ab1affba73baa38ecf134ed310728911edb7ddc
Umut Tavşankoru <um155ut@hotmail.com>
2020-11-19 19:29:18 +0300

    0.8.1[Removed not used files and created README.md]

--- Commands/ecobase.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ecobase',
-    execute(message, args){
-        let target = message.mentions.users.first() || message.author
-
-        const targetID = target.id
-    }
-}
